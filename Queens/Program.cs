4160a062177191c0a7e631e6eb0bda501e8256f8
chaudi <cederfelt@live.com>
2016-04-02 20:36:39 +0200

    The files

---
+++ Queens/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace Queens
+{
+    class Program
+    {
+        private const int DEFAULTSIZE = 14;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Input size, if blank it will be {DEFAULTSIZE}");
+
+            int size;
+
+            if (!int.TryParse(Console.ReadLine(), out size))
+            {
+                size = DEFAULTSIZE;
+            }
+
+            Threaded(size);
+            NotThreaded(size);
+            Console.ReadLine();
+        }
+
+        private static void Threaded(int size)
+        {
+            var taskPositions = new int[size][];
+            int solutions;
+
+            Solver s = new Solver();
+            System.Diagnostics.Stopwatch watch = new System.Diagnostics.Stopwatch();
+            watch.Start();
+
+            for (int i = 0; i < taskPositions.Length; i++)
+            {
+                taskPositions[i] = new int[size];
+                taskPositions[i][0] = i;
+            }
+
+            var tList = taskPositions.Select((t, i) => i).Select(i1 => s.SolveThreaded(1, taskPositions[i1])).Cast<Task>().ToList();
+            Task.WaitAll(tList.ToArray());
+            solutions = tList.Sum(task => ((Task<int>)task).Result);
+
+            watch.Stop();
+
+            double elapsedMs = watch.ElapsedMilliseconds;
+            Console.WriteLine("Threaded:");
+            Console.WriteLine($"Solutions {solutions}");
+            Console.WriteLine($"Elapsed milliseconds {elapsedMs}");
+        }
+
+        private static void NotThreaded(int size)
+        {
+
+            var positions = new int[size];
+            int solutions;
+            Solver s = new Solver();
+            System.Diagnostics.Stopwatch watch = new System.Diagnostics.Stopwatch();
+            watch.Start();
+
+            solutions = s.SolveUnThreaded(0, positions);
+
+            watch.Stop();
+            double elapsedMs = watch.ElapsedMilliseconds;
+            Console.WriteLine("Not Threaded:");
+            Console.WriteLine($"Solutions {solutions}");
+            Console.WriteLine($"Elapsed milliseconds {elapsedMs}");
+
+        }
+    }
+}
---
+++ Queens/Solver.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Queens
+{
+    public class Solver
+    {
+
+        public int SolveUnThreaded(int row, int[] positions)
+        {
+            int solutions = 0;
+            solutions = Backtrack(row, positions, solutions);
+            return solutions;
+        }
+
+        public async Task<int> SolveThreaded(int row, int[] positions)
+        {
+            int solutions = 0;
+            solutions = await Task.Run(() => Backtrack(row, positions, solutions));
+            return solutions;
+        }
+
+        public int Backtrack(int row, int[] positions, int solutions)
+        {
+            if (row == positions.Length)
+            {
+                solutions++;
+                return solutions;
+            }
+            else
+            {
+                for (int i = 0; i < positions.Length; i++)
+                {
+                    positions[row] = i;
+                    if (IsValid(positions, row))
+                    {
+                        solutions = Backtrack(row + 1, positions, solutions);
+                    }
+                    else
+                    {
+
+                    }
+                }
+            }
+            
+            return solutions;
+        }
+
+        public Boolean IsValid(int[] positions, int currentRow)
+        {
+            for (int j = 0; j < currentRow; j++)
+            {
+                if (positions[j] == positions[currentRow] || positions[j] == positions[currentRow] - (currentRow - j) ||
+                    positions[j] == positions[currentRow] + (currentRow - j))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
